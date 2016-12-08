2adeb73a4c95e5c87a5a8c417c20f82c927566b2
Dixon <u390892@ent.wfb.bank.corp>
2016-12-07 16:29:18 -0800

    Switching to a more modular theme (minimal mistakes), that is under more active development

---
+++ README.md
@@ -0,0 +1,1 @@
+http://launchaco.com/build/
--- _build/build.py
+++ _build/build.py
@@ -2,10 +2,13 @@ import os
 import pandas as pd
 import numpy as np
 
+# define input and output file locations
 fileInPath = os.path.join(os.path.dirname(__file__), "../_data/atdb.092116023143.ALL.csv")
 fileOutPath = os.path.join(os.path.dirname(__file__), "../_data/nn_out.csv")
 
 df = pd.read_csv(fileInPath, index_col='id')
+# miles_hiked = df.loc[df['dt_reach'] == 1, 'to_spgr'].sum()
 features = df[df['type'] == 'FEATURE']
 
+# write output
 features.to_csv(fileOutPath)
