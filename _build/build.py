73dae4ebbd175e2699b6a858bbc1e7d2c2dacc58
Daniel <u390892@ent.wfb.bank.corp>
2016-12-19 11:21:21 -0800

    Fix to static map image
    Also attempts to add hiker icon to mark current location.

--- _build/build.py
+++ _build/build.py
@@ -1,14 +1,63 @@
 import os
 import pandas as pd
-import numpy as np
+import json
 
 # define input and output file locations
-fileInPath = os.path.join(os.path.dirname(__file__), "../_data/atdb.092116023143.ALL.csv")
-fileOutPath = os.path.join(os.path.dirname(__file__), "../_data/nn_out.csv")
+fileInPath = '../_data/atdb.092116023143.ALL.csv'
+fileOutPath = '../_data/stats.json'
 
-df = pd.read_csv(fileInPath, index_col='id')
-# miles_hiked = df.loc[df['dt_reach'] == 1, 'to_spgr'].sum()
-features = df[df['type'] == 'FEATURE']
+# column names
+dtCol = 'dt_reached'
 
-# write output
-features.to_csv(fileOutPath)
+
+def get_current_location(df):
+    completed = df[df[dtCol].notnull()][['lat', 'lon', dtCol]]
+    completed[dtCol] = completed[dtCol].dt.strftime('%Y-%m-%d')
+    last_completed = completed.iloc[-1]
+
+    return {'current_location': last_completed.to_dict()}
+
+
+def get_miles_hiked_per_day(df):
+    completed = df[df[dtCol].notnull()][[dtCol, 'to_spgr']]
+    completed['dt'] = completed[dtCol].dt.strftime('%Y-%m-%d')
+
+    # need to shift the values, since the last checkpoint of each day should be the first checkpoint of the next day
+    completed['to_spgr_shifted'] = completed.to_spgr.shift(1)
+
+    # group by day
+    group_day = completed.groupby('dt')
+
+    f = {'to_spgr': 'last', 'to_spgr_shifted': 'first'}
+    miles_per_day = group_day.agg(f)
+    miles_per_day['miles'] = miles_per_day.to_spgr - miles_per_day.to_spgr_shifted
+
+    return {'miles_per_day': miles_per_day['miles'].to_dict()}
+
+
+def get_estimated_completion(df):
+    features = df[df['type'] == 'FEATURE']
+
+    return {'estimated_completion': '2017-08-15'}
+
+
+df = pd.read_csv(fileInPath, index_col='id', parse_dates=[dtCol], infer_datetime_format=True)
+# print(df[:5])
+# print("\n\n")
+
+# buffer to store output
+out = []
+
+
+cl = get_current_location(df)
+mpd = get_miles_hiked_per_day(df)
+ec = get_estimated_completion(df)
+
+
+print("\n")
+print(json.dumps([cl, mpd, ec]))
+
+
+# write json file
+with open(fileOutPath, 'w') as outfile:
+    json.dump([cl, mpd, ec], outfile)
--- _build/generate_map_img.py
+++ _build/generate_map_img.py
@@ -2,27 +2,37 @@ import os
 import pandas as pd
 from urllib.request import urlopen
 
+
+# file locations
 wd = os.path.dirname(__file__)
 locationsPath = os.path.join(wd, "../_data/atdb.092116023143.ALL.csv")
+statsFilePath = os.path.join(wd, "../_data/stats.json")
 imgOutPath = os.path.join(wd, "../img/googlemap.png")
 
+# map options
 baseurl = "https://maps.googleapis.com/maps/api/staticmap?"
 options = "size=640x350&zoom=5&scale=2"
+marker = "markers=icon:https://goo.gl/otDBv3"
 style = "style=feature:landscape|lightness:40&style=feature:road|visibility:simplified"
 api_key = "key=AIzaSyCpNlAW16ash3_DakQeeIwzPs5KxTz_lmI"
 
-# Read in locations from csv file
+# read in locations from csv file
 locations = pd.read_csv(locationsPath, index_col='id')
 shelters = locations[locations['type'] == 'SHELTER']
 
-# Append the lat-long of each shelter to the url path
+# append the lat-long of each shelter to the url path
 path = 'path=weight:2'
 for index, row in shelters.iterrows():
     lat = "{:.4f}".format(row['lat'])
     long = "{:.4f}".format(row['lon'])
     path = path + '|' + lat + ',' + long
 
-url = baseurl + options + '&' + style + '&' + path  + '&' + api_key
+# get the last know current location
+completed = locations[locations['dt_reached'].notnull()][['lat', 'lon']].iloc[-1]
+marker = marker + '|' + str(completed['lat']) + ',' + str(completed['lon'])
+
+# build final url
+url = baseurl + options + '&' + marker + '&' + style + '&' + path + '&' + api_key
 
 # The Google Maps api has a max length on urls.  Make sure our url is shorter than that
 assert(len(url) < 8192)
